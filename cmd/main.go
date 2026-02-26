1d2a84d93e170b825afac6c36ba77f57d65d4d15
Lorenz Hohermuth <lorenz.hohermuth@bsi-software.com>
2026-02-26 16:50:56 +0100

    bigger setup

--- cmd/main.go
+++ cmd/main.go
@@ -14,11 +14,11 @@ type model struct {
 
 
 func main() {
-    p := tea.NewProgram(initialModel())
-    if _, err := p.Run(); err != nil {
-        fmt.Printf("Alas, there's been an error: %v", err)
-        os.Exit(1)
-    }
+  p := tea.NewProgram(initialModel())
+  if _, err := p.Run(); err != nil {
+  	fmt.Printf("Alas, there's been an error: %v", err)
+    os.Exit(1)
+  }
 }
 
 func initialModel() model {
