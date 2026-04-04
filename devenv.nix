00ac98f50be6b2f0f5427afa0ed03f581ac37b5b
Lorenz Hohermuth <lofloho@proton.me>
2026-04-04 18:27:34 +0200

    wtf gio works

--- devenv.nix
+++ devenv.nix
@@ -10,20 +10,40 @@
   packages = [
     pkgs.git
     pkgs.go
+
+    pkgs.pkg-config
+
     pkgs.xorg.libX11
     pkgs.xorg.libXrandr
     pkgs.xorg.libXinerama
     pkgs.xorg.libXcursor
     pkgs.xorg.libXi
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libxcb
+
+    pkgs.libxkbcommon
+
+    pkgs.wayland
+
+    pkgs.libGL
+    pkgs.vulkan-headers
+    pkgs.vulkan-loader
   ];
 
   languages.go.enable = true;
 
   env.LD_LIBRARY_PATH = lib.makeLibraryPath [
+    pkgs.libGL
+    pkgs.vulkan-loader
+    pkgs.libxkbcommon
+
+    pkgs.wayland
+
     pkgs.xorg.libX11
     pkgs.xorg.libXrandr
     pkgs.xorg.libXinerama
     pkgs.xorg.libXcursor
     pkgs.xorg.libXi
+    pkgs.xorg.libxcb
   ];
 }
